55119550a9645f65ed3c674a430d6ee70601b129
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-02-18 00:53:18 +0530

    created routes for all endpoints

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -6,4 +6,16 @@ const transactionController = require('../controllers/transactionController')
 router.route('/')
       .get(transactionController.getAllTransactions)
 
+router.route('/stats')
+      .get(transactionController.getStats)
+
+router.route('/bar-chart')
+      .get(transactionController.getBarChartData)
+
+router.route('/pie-chart')
+      .get(transactionController.getPieChartData)
+
+router.route('/monthly-stats')
+      .get(transactionController.getMonthlyStats)
+
 module.exports = router
